bdd7f22d202a7feaa481679e513256e5117b3cf3
Igor <iksendikov@gmail.com>
2021-06-30 14:25:51 +0300

    added new app and home.html

--- stepicBlog/settings.py
+++ stepicBlog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appBlog',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'stepicBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates/stepicBlog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
